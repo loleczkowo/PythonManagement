44e88bdb8bf7d5219cd563f7073c71d51ac37208
SK <szymon.kondratek@gmail.com>
2025-12-03 13:26:56 +0100

    example update

--- services_example/__init__.py
+++ services_example/__init__.py
@@ -1,8 +1,13 @@
-from .registry import services
+from core.logs import log, QINFO
 
 from . import basic_service
 from . import multiple_services
 from . import machine_specific
+
+from .registry import services
+
+services_names = "\n".join([service.name for service in services])
+log(QINFO, f"Current services: \n{services_names}")
 
 __all__ = [
     "services", "basic_service", "multiple_services", "machine_specific"
